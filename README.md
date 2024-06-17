92282c9a00ae748fe6d54987c22b871a4fd7db4f
aaminha <dks4857@gmail.com>
2024-06-17 22:33:35 +0900

    ✨ 튜토리얼 Chapter 3

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 - [Chapter 1](./docs/Chapter1.md)
 - [Chapter 2](./docs/Chapter2.md)
+- [Chapter 3](./docs/Chapter3.md)
---
+++ app/ui/font.ts
@@ -0,0 +1,9 @@
+// 애플리케이션 전체에서 사용될 글꼴 유지
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
